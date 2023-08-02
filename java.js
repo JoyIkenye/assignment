2fb30b0a53c13c90c962427307778868509ba5f2
JoyIkenye <ikenyejessica@gmail.com>
2023-08-02 16:04:26 +0100

    initial commit

--- java.js
+++ java.js
@@ -1,29 +1,42 @@
 // const year= 2016;
-const year= new Date().getFullYear();
- 
-
-if (year % 4===0 && year % 100!==0){
-    alert ("It's a leap year")
-}else if ("year % 400===0") {
-    alert ("It's not a leap year")
-}else{
-    alert ("It's not a leap year")
-}
-
-// const integer=22;
-
-// if(integer % 3===0 && integer % 5===0){
-//     alert ("FizzBuzz")
-// } else if (integer % 3===0){
-//     alert ("Buzz")
-// }else if (integer % 5===0) {
-//     alert ("Fizz")
-// }else {
-//     alert ("not divisible by both 5 and 3")
+// const year= new Date().getFullYear();
+//  
+// 
+// if (year % 4===0 && year % 100!==0 || year % 400===0){
+    // alert ("It's a leap year")
+    // 
+// }else{
+    // alert ("It's not a leap year")
 // }
 
+// const integer=9;
+// 
+// if(integer % 3===0 && integer % 5===0){
+    // alert ("FizzBuzz")
+// } else if (integer % 3===0){
+    // alert ("Buzz")
+// }else if (integer % 5===0) {
+    // alert ("Fizz")
+// }else {
+    // alert ("not divisible by both 5 and 3")
+// }
 
+// Print even numbers from 1 to 20 in the console using the for loop
+// for (let i=2; i<=20; i+=2){
+    // console.log(i);
+// }
 
+//Calculate the sum of numbers between 1 and 10 using the for loop
+// let sum=0
+// for (let i=1; i<=10; i++) {
+//     sum += i   
+// }
+// console.log(i);
 
+//Write a script that prints the multiplication table of a number 2  in the following format:
+for (let i=1; i<= 10; i++) {
+    let result= 2*i;
+    console.log(`${2} * ${i} =${result}`)
+}
 
 
