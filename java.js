558b1783149eddebd67576d8f0a3034bed15820c
unknown <ebojimichael@gmail.com>
2023-08-02 09:20:48 +0100

    my first commit

---
+++ java.js
@@ -0,0 +1,29 @@
+// const year= 2016;
+const year= new Date().getFullYear();
+ 
+
+if (year % 4===0 && year % 100!==0){
+    alert ("It's a leap year")
+}else if ("year % 400===0") {
+    alert ("It's not a leap year")
+}else{
+    alert ("It's not a leap year")
+}
+
+// const integer=22;
+
+// if(integer % 3===0 && integer % 5===0){
+//     alert ("FizzBuzz")
+// } else if (integer % 3===0){
+//     alert ("Buzz")
+// }else if (integer % 5===0) {
+//     alert ("Fizz")
+// }else {
+//     alert ("not divisible by both 5 and 3")
+// }
+
+
+
+
+
+
