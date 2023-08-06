db1e2e940f4f328be333b9c2f6c6e87d6aa355f3
JoyIkenye <ikenyejessica@gmail.com>
2023-08-06 16:56:59 +0100

    my first commit

--- java.js
+++ java.js
@@ -34,9 +34,53 @@
 // console.log(i);
 
 //Write a script that prints the multiplication table of a number 2  in the following format:
-for (let i=1; i<= 10; i++) {
-    let result= 2*i;
-    console.log(`${2} * ${i} =${result}`)
-}
+// for (let i=1; i<= 10; i++) {
+    // let result= 2*i;
+    // console.log(`${2} * ${i} =${result}`)
+// }
+
+//Create an array of numbers with the values [1,3,5,7,9]
+const myNumber= [1,3,5,7,9]
+//1. Add the value 11 to the end of the array
+
+// myNumber.push(11) 
+    // console.log(myNumber);
+
+//2. Remove the first element from the array
+
+// myNumber.shift(1)
+// console.log(myNumber)
+
+//3. Replace the value at index 2 with 6
+
+// myNumber.splice(2,1,6)
+// console.log(myNumber);
+
+//4. Check if the array contains the value 5. Print "value found!" if it does, otherwise, print "value not found!"
+
+// if(myNumber.includes(5)) {
+    // alert ("value found")
+// }else{
+    // alert ("value not found")
+// }
+
+//5. use loop to print all the elements of the array
+// for(let i=1;i<=10;i+=2) {
+    // console.log(i);
+// }
+
+//Create an array of students with two objects representing two students. Each object should have properties like name, age, and grade.
+const students = [
+    { name: "Temi", age: 18, grade: "A" },
+    { name: "Jude", age: 17, grade: "B" }
+  ];
+  for(let i=0; i<students.length;i++) {
+    const student= students [i];
+    const upperCase= student.name.toUpperCase();
+    const age= student.age;
+    const gradeStu= student.grade;
+    console.log(`Student ${i + 1}: Name: ${upperCase}, Age: ${age},Grade:${gradeStu}`)
+  }
+
 
 
